5a3d22626f077502fc06da5d60d02da803307f26
benjamin montet <benjamin.montet@gmail.com>
2024-08-07 16:00:52 +0200

    rails-yelp-mvp

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+
+puts "destroy all the restaurants"
+Restaurant.destroy_all
+puts "All restaurants destroyed"
+
+puts "Creating restaurants"
+Restaurant.create!(name: "Chez Fufu", address: "15 Rue des Alouettes", phone_number: "0610101010", category: "belgian")
+Restaurant.create!(name: "Allons enfants", address: "Rue du Faubours", phone_number: "0612456598", category: "belgian")
+Restaurant.create!(name: "Les allumettes", address: "Impasse de la lune", phone_number: "0678879865", category: "french")
+Restaurant.create!(name: "Rince gosier", address: "Avenue Foch", phone_number: "0654632154", category: "italian")
+Restaurant.create!(name: "21 Jump street", address: "Au rond point à droite", phone_number: "0689653245", category: "chinese")
+puts "Restaurants created"
+
+# ["chinese", "italian", "japanese", "french", "belgian"]
